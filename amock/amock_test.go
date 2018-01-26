8d24af61cb58ddef4126eee52a985553cff1b849
Thomas Schaffer <loopfz@gmail.com>
2018-01-26 13:05:42 +0100

    Amock: refactor OnFunc filter (#30)
    amock: refactor OnFunc filter

--- amock/amock_test.go
+++ amock/amock_test.go
@@ -10,43 +10,83 @@ import (
 func TestMock(t *testing.T) {
 
 	mock := NewMock()
-	mock.Expect(foo.GetFoo, 200, foo.Foo{Identifier: "f1234", BarCount: 42}).OnIdentifier("f1234")
+	mock.Expect(200, foo.Foo{Identifier: "f1234", BarCount: 42}).OnIdentifier("f1234").OnFunc(foo.GetFoo)
 	foo.Client.Transport = mock
 
-	fmt.Println("Step 1: get a foo with an identifier not matching the expected one")
+	fmt.Println("get a foo with an identifier not matching the expected one")
 
 	f, err := foo.GetFoo("f1")
 	if err == nil {
 		t.Error("Should not have returned foo object with non-matching ident")
 	}
 
-	fmt.Println("Step 1 returned:", err)
+	fmt.Println("returned:", err)
 
 	fmt.Println("----------------------------------------------------------------------")
 
-	fmt.Println("Step 2: get a foo with the correct identifier")
+	fmt.Println("get a foo with the correct identifier but going through an unexpected code path")
+
+	f, err = foo.GetFoo2("f1234")
+	if err == nil {
+		t.Error("Should not have returned foo object with non-matching func")
+	}
+
+	fmt.Println("returned:", err)
+
+	fmt.Println("----------------------------------------------------------------------")
+
+	fmt.Println("get a foo with the correct identifier")
 
 	f, err = foo.GetFoo("f1234")
 	if err != nil {
 		t.Error(err)
 	}
 
-	fmt.Println("Step 2 returned:", f)
+	fmt.Println("returned:", f)
 
 	fmt.Println("----------------------------------------------------------------------")
 
-	fmt.Println("Step 3: make the mock simulate a 503, get a foo expecting an error")
+	fmt.Println("update foo object and call an update _method_")
 
-	mock.Expect(foo.GetFoo, 503, Raw([]byte(`<html><body><h1>503 Service Unavailable</h1>
-No server is available to handle this request.
-</body></html>`)))
+	f.BarCount = 43
 
-	f, err = foo.GetFoo("f2")
-	if err == nil {
+	mock.Expect(200, f).OnIdentifier(f.Identifier).OnFunc(f.UpdateFoo)
+
+	f, err = f.UpdateFoo()
+	if err != nil {
 		t.Error(err)
 	}
 
-	fmt.Println("Step 3 returned:", err)
+	fmt.Println("returned:", f)
+
+	fmt.Println("----------------------------------------------------------------------")
+
+	fmt.Println("make the mock simulate a 503, get a foo expecting an error")
+
+	mock.Expect(503, Raw([]byte(`<html><body><h1>503 Service Unavailable</h1>
+No server is available to handle this request.
+</body></html>`))).Sticky()
+
+	f, err = foo.GetFoo("f2")
+	if err == nil {
+		t.Error("expected a 503 error")
+	}
+
+	fmt.Println("returned:", err)
+
+	fmt.Println("----------------------------------------------------------------------")
+
+	fmt.Println("previous response is sticky, retry and expect same error")
+
+	f, err2 := foo.GetFoo("f2")
+	if err2 == nil {
+		t.Error("expected a 503 error")
+	}
+	if err.Error() != err2.Error() {
+		t.Errorf("Errors mismatched: '%s' // '%s'", err.Error(), err2.Error())
+	}
+
+	fmt.Println("returned:", err2)
 
 	mock.AssertEmpty(t)
 }
--- amock/foo/foo.go
+++ amock/foo/foo.go
@@ -1,6 +1,7 @@
 package foo
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -15,6 +16,14 @@ type Foo struct {
 }
 
 func GetFoo(ident string) (*Foo, error) {
+	return doGetFoo(ident)
+}
+
+func GetFoo2(ident string) (*Foo, error) {
+	return doGetFoo(ident)
+}
+
+func doGetFoo(ident string) (*Foo, error) {
 	resp, err := Client.Get("http://www.foo.com/foo/" + ident)
 	if err != nil {
 		return nil, err
@@ -31,5 +40,33 @@ func GetFoo(ident string) (*Foo, error) {
 	if err != nil {
 		return nil, err
 	}
+	return ret, nil
+}
+
+func (f *Foo) UpdateFoo() (*Foo, error) {
+
+	body, err := json.Marshal(f)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("PUT", "http://www.foo.com/foo/"+f.Identifier, bytes.NewReader(body))
+	resp, err := Client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode < 200 || resp.StatusCode >= 400 {
+		return nil, fmt.Errorf("got http error %d", resp.StatusCode)
+	}
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	ret := &Foo{}
+	err = json.Unmarshal(body, ret)
+	if err != nil {
+		return nil, err
+	}
+
 	return ret, nil
 }
